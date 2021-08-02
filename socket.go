ac002def725b1c5ada85b18c30becfb00252f28e
Martin <martinbromikkelsen.com>
2021-08-02 20:23:30 +0200

    first

---
+++ socket.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	fmt.Println("hello world")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.HandleFunc("/v1/ws", func(w http.ResponseWriter, r *http.Request) {
+		var conn, _ = upgrader.Upgrade(w, r, nil)
+		go func(conn *websocket.Conn) {
+			for {
+				mType, msg, _ := conn.ReadMessage()
+
+				conn.WriteMessage(mType, msg)
+			}
+		}(conn)
+	})
+}
